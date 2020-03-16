7a0d7cb3af470e1fe3d4732c0417127836c94fd9
Braden Collingwood <bradencollingwood82@gmail.com>
2020-03-15 23:19:04 -0400

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './assets/index.css'
+import splashImage from './assets/images/boat.png';
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="flex h-screen">
+        <div className="w-1/2 h-full">
+
+        </div>
+        <div className="w-1/2 h-full bg-blue-500 bg-cover bg-right" style={{backgroundImage:`url(${splashImage})`,backgroundPosition: 'right 12% bottom 0'}}>
+
+        </div>
+      </div>
     </div>
   );
 }
